c48fc917c059f04404b8c03ee3af55a38a27be2e
nguyenvananh1990 <nguyenvananhptithcm@gmail.com>
2019-10-04 21:21:41 +0700

    added 02

--- src/com/helloworld/controller/HelloWorld.java
+++ src/com/helloworld/controller/HelloWorld.java
@@ -2,6 +2,6 @@ package com.helloworld.controller;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello I am a development 01");
+		System.out.println("Hello I am a development 02");
 	}
 }
